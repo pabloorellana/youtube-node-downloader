31d4c4ce7d1285a8ac00c5183726232e8b834af3
Pablo Orellana T <pablo.orellana.torrez@gmail.com>
2015-12-12 03:11:41 -0400

    Refactoring to use eachSeriesSettled Adding simple track mechanism to detect already downloaded videos Refactoring downloader

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var downloader = require('./lib/downloader.js');
+var downloader = require('./lib/downloader.js'),
+  youtubeApi = require('./lib/youtube-api.js'),
+  downloader = require('./lib/downloader.js'),
+  utils = require('./lib/utils.js'),
+  async = require('./lib/async.js');
 
 var flag = process.argv[2];
 
 var url = process.argv[3]
 
 var downloadTargetMap = {
-  '-plist': downloader.downloadVideosByPlayListId,
-  '-channel': downloader.downloadVideosByChannelId,
-  '-video': downloader.downloadVideoById
+  '-plist': downloadVideosByPlayListId,
+  '-channel': downloadVideosByChannelId,
+  '-video': downloadVideoById
 };
 
 var handler = downloadTargetMap[flag];
@@ -16,6 +20,46 @@ if (!handler) {
   return;
 }
 
-handler(url).catch(function (err) {
-  console.log(err);
-});
+handler(url).then(a);
+
+function a (results) {
+  var completed = results.filter((result) => {
+    return result.state === 'resolved';
+  }).length;
+
+  var failed = results.filter((result) => {
+    return result.state === 'rejected';
+  });
+
+  console.log('\n' + 'Completed:\t' + completed);
+  console.log('Failed:\t' + failed.length + '\n');
+
+  if (failed.length !== 0 ) {
+    console.log('Details:' + '\n');
+  }
+
+  failed.forEach((reason) => {
+    console.log(reason.value + '\n');
+  });
+}
+
+function downloadVideosByPlayListId (playlistIds) {
+  return youtubeApi.getPlayLists(playlistIds).then((playlist) => {
+    utils.showPlayListInfo(playlist);
+    return async.eachSeriesSettled(playlist, downloader.download);
+  })//.then(a);
+};
+
+function downloadVideosByChannelId (channelIds) {
+  return youtubeApi.getChannels(channelIds).then((channelList) => {
+    return async.eachSeriesSettled(channelList, downloadVideosByPlayListId);
+  });
+};
+
+function downloadVideoById (videoIds) {
+  return youtubeApi.getVideos(videoIds).then((videos) => {
+    return async.eachSeriesSettled(videos, downloader.download);
+  }).then(a);
+
+};
+
